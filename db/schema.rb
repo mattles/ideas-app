072b665eaba0a763b21d70292216a3af94253f8a
Matt Hunter <matthunter203@gmail.com>
2017-10-26 18:46:38 +1300

    Add the comments scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023084412) do
+ActiveRecord::Schema.define(version: 20171026051032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.bigint "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 20171023084412) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "ideas"
 end
